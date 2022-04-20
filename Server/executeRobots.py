63c9876ea322a8be80c8c2b6cd5cd60bff4ae95b
Luan <lujetix@gmail.com>
2022-04-20 09:37:20 -0300

    If robot not exists, dont try run it and set return to 'File not exists'

--- Server/executeRobots.py
+++ Server/executeRobots.py
@@ -65,6 +65,9 @@ for call in calls.itertuples():
 
         #If the robot file exists
         if os.path.exists(robot_path):
+            '''
+                OPEN ROBOT FILE
+            '''
             #Get the robot file extension
             robot_extension = robot_path.split('.')[-1]
 
@@ -91,30 +94,45 @@ for call in calls.itertuples():
             elif robot_extension == 'xlsm':
                 #Execute the excel with the command 'excel'
                 os.system('excel {}'.format(robot_path))
+            
+            '''
+                PARAMETERS FILE
+            '''
+            #If the robot has parameters
+            if robot.with_parameters_file == 1:
+                #wait the file has the text 'usado'
+                while True:
+                    #Open the file
+                    parameters_file = open(parameters_file_path, 'r')
+                    #Read the file
+                    parameters_file_text = parameters_file.read()
+                    #Close the file
+                    parameters_file.close()
+
+                    #If the file has the text 'usado'
+                    if 'usado' in parameters_file_text:
+                        #change the started_at of the call to now in sql format
+                        conn.execute("UPDATE calls SET started_at = ? WHERE id = ?", (time.strftime('%Y-%m-%d %H:%M:%S'), call.id))
+                        #commit the changes
+                        conn.commit()
+
+                        #break the loop
+                        break
+                    
+                    #Wait 1 second
+                    time.sleep(1)
+        else:
+            #json_return = '{"html":"Arquivo do robô não encontrado, contate o programador."}'
+            json_return = '{"html":"Arquivo do robô não encontrado, contate o programador."}'
+            #now in sql format
+            now = time.strftime('%Y-%m-%d %H:%M:%S')
+            #change the started_at and ended_at of the call to now in sql format
+            conn.execute("UPDATE calls SET started_at = ?, ended_at = ?, json_return = ? WHERE id = ?", (now, now, json_return, call.id))
+            #commit the changes
+            conn.commit()
+
+            #print the error
+            print("Arquivo do robô '", robot_path, "' não encontrado.")
 
         #Commit the changes
         conn.commit()
-
-        #If the robot has parameters
-        if robot.with_parameters_file == 1:
-            #wait the file has the text 'usado'
-            while True:
-                #Open the file
-                parameters_file = open(parameters_file_path, 'r')
-                #Read the file
-                parameters_file_text = parameters_file.read()
-                #Close the file
-                parameters_file.close()
-
-                #If the file has the text 'usado'
-                if 'usado' in parameters_file_text:
-                    #change the started_at of the call to now in sql format
-                    conn.execute("UPDATE calls SET started_at = ? WHERE id = ?", (time.strftime('%Y-%m-%d %H:%M:%S'), call.id))
-                    #commit the changes
-                    conn.commit()
-
-                    #break the loop
-                    break
-                
-                #Wait 1 second
-                time.sleep(1)
