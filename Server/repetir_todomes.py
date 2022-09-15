0990e21ce3eb8cd968f2ecfc6dbae291e9b6c337
Luan <lujetix@gmail.com>
2022-09-15 12:40:29 -0300

    removido execuções mensais

--- Server/repetir_todomes.py
+++ Server/repetir_todomes.py
@@ -8,6 +8,6 @@ today = time.strftime("%Y-%m-%d %H:%M:%S")
 calls_to_execute = []
 
 #array call with robot = 126, user = ti01, 'created_at' = now, json_parameters = {}
-calls_to_execute.append({'robot': 126, 'user': 'ti01', 'created_at': today, 'json_parameters': '{}'})
+#calls_to_execute.append({'robot': 126, 'user': 'ti01', 'created_at': today, 'json_parameters': '{}'})
 
 execute_calls(calls_to_execute)
