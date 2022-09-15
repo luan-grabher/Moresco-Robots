395d518faecc2508dfe012d7116afa4efb7d47f6
Luan <lujetix@gmail.com>
2022-09-15 12:24:44 -0300

    execução do comando alterado para subprocess

--- Server/executeRobots.py
+++ Server/executeRobots.py
@@ -156,7 +156,7 @@ for call in calls.itertuples():
                     Robot(call.id) #instantiate a new robot automatically set the started_at as now
                 
                 #Call the command
-                os.system(command)
+                subprocess.call(command, shell=True)
                 output = 'Nenhum Retorno'
                 
                 '''
